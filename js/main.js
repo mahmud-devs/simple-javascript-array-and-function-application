c8c748b9e2511dfd093143f4655f3b9d4d484ab8
mahmud-devs <mahmudulislam0999@gmail.com>
2024-02-04 22:43:49 +0600

    function

--- js/main.js
+++ js/main.js
@@ -3,11 +3,44 @@
 ============= array and function =============
 
 */
-let array = [3, 5, 6, 7, 11, 77, 82, 8, 26, 37, nam, okkkk, oggggg, hmmmm];
+let array = [
+    3,
+    5,
+    6,
+    7,
+    11,
+    77,
+    82,
+    8,
+    26,
+    37,
+    "nam",
+    "pojonon",
+    "kojedviohuav",
+    "oudbviebv",
+];
 
 for (i = 0; i < array.length; i++) {
-    if(array[i] === 'nam, okkkk, oggggg, hmmmm'   ){
+    if ((array[i] === "nam", "pojonon", "kojedviohuav", "oudbviebv")) {
         array[i] = 50;
+    } else {
     }
     console.log(`${i} X ${array[i]} = ${i * array[i]}`);
+}
+
+/*
+
+    ================== function =================
+
+*/
+
+function namta(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+namta(55, 98);
+
+for (i = 0; i <= 10; i++) {
+    console.log(`${i} X ${sum} = ${i*sum}`);
 }
